c26695adcd05b75dfa59ba0a538920a82ff3df41
Duncan Robertson <duncan@kyan.com>
2019-02-15 08:15:51 +0000

    Initial Commit

---
+++ Oi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Oi
+//
+//  Created by Duncan Robertson on 15/02/2019.
+//  Copyright © 2019 Duncan Robertson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Oi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Oi
+//
+//  Created by Duncan Robertson on 15/02/2019.
+//  Copyright © 2019 Duncan Robertson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
